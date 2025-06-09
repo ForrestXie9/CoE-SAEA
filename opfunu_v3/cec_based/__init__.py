f282862a88178556c117dc62fb8c9d493bf5e1db
Lindong Xie <138126615+ForrestXie9@users.noreply.github.com>
2025-06-09 10:43:37 +0800

    Update __init__.py

--- opfunu_v3/cec_based/__init__.py
+++ opfunu_v3/cec_based/__init__.py
@@ -5,15 +5,7 @@
 # --------------------------------------------------%
 
 from .cec2005 import *
-from .cec2008 import *
-from .cec2010 import *
-from .cec2013 import *
-from .cec2014 import *
+
 from .cec2015 import *
-from .cec2017 import *
-from .cec2019 import *
-from .cec2020 import *
-from .cec2021 import *
-from .cec2022 import *
 
 __all__ = [s for s in dir() if not s.startswith('_')]
