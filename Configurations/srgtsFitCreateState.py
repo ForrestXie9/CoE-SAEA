4502ff1319d52a61a465eec7bd9aac470078c982
Lindong Xie <138126615+ForrestXie9@users.noreply.github.com>
2025-06-09 09:50:39 +0800

    Delete Configurations directory

--- Configurations/srgtsFitCreateState.py
+++
@@ -1,5 +0,0 @@
-def srgtsFitCreateState(srgtOPT):
-    srgtSTT = {}
-    srgtSTT['FIT_Fn'] = srgtOPT['FIT_Fn']
-    srgtSTT['FIT_FnVal'] = float('nan')
-    return srgtSTT
--- Configurations/srgtsPRSEvaluate.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from Configurations.srgtsPRSCreateGramianMatrix import srgtsPRSCreateGramianMatrix
-def srgtsPRSEvaluate(x, srgtSRGT):
-    # Simulate srgtSRGT in X
-    X = srgtsPRSCreateGramianMatrix(x, srgtSRGT['NbVariables'], srgtSRGT['PRS_Degree'], srgtSRGT['PRS_RemovedIdx'])
-    yhat = np.dot(X, srgtSRGT['PRS_Beta'])
-    return yhat
